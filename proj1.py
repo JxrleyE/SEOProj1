200f9a010e19e8e73d4c88973e776f7a7a2a6ace
Jarley Escobar <escobarjarley@gmail.com>
2025-06-29 00:34:41 +0000

    Loading API DATA into a DB

--- proj1.py
+++ proj1.py
@@ -1,10 +1,12 @@
 import requests
+import sqlalchemy as db
+import pandas as pd
 
-token = "BQBSAkwdCTrHkHkI-BXVkuPCo81ktPhSNhLlYF3Bx03laQV9d0sd11DTnP4lD6ZTvC2sT6Yde6jQt9ccb8woq7IlhYwilryqgPdDj2dCInAFp10aqcHGMkHWZ4Pp_KHqxt6Yqnmu_gQ"
+token = "BQD_FbxCYtfnOeBB4rFbZBgmiYzLmRevrLoN7w-uPyNCvuaOkHopVGLMWZBM51UULVeN7KmLrl3C0Bvwo6uZptlUzvax7bO3mSgJBy2X2Oz7YEVCj2qynPL4_XWFz0VQvSZJV43_cEQ"
 
 id = "3Z7onAknzpinUu3KtmgeZb?si=f1d98XYWT3aUadCfkI8krQ"
 
-url = f"https://api.spotify.com/v1/artists/{id}"
+url = f"https://api.spotify.com/v1/artists/{id}/top-tracks"
 
 headers = {
   "Authorization": f"Bearer {token}"
@@ -14,7 +16,14 @@ response = requests.get(url, headers=headers)
 
 data = response.json()
 
-print(data)
+newDF = pd.DataFrame.from_dict(data['genres'])
 
+engine = db.create_engine('sqlite:///TEST1.db')
+
+newDF.to_sql('table_name', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+   print(pd.DataFrame(query_result))
 
 
